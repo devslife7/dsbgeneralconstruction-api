6503d38f4a3efb27fbb1ddd81dd3dbffe3a84cd9
Marcos Velasco <devslife7@gmail.com>
2023-07-20 22:51:02 -0400

    added seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Work.destroy_all
+
+Work.create(title: "title 1", description: "description1")
+
+puts "Succesfull Seed!!"
